db811b67d3c244acbe8dd4fd90c0dc932ed9a2f9
Vishal Kotcherlakota <vishal@kotcherlakota.org>
2016-09-26 22:33:43 -0700

    got authentication working

--- node/server.js
+++ node/server.js
@@ -54,6 +54,31 @@ apiRouter.post("/register", function(request, response) {
     }
 });
 
+apiRouter.post("/auth", function(request, response) {
+    User.findOne({name: request.body.name}, function(err, user) {
+        if (err) throw err;
+        if (!user) {
+            // used to mask shorter execution time due to user not existing
+            // without this, it's easy to tell whether or not a user exists by looking at
+            // the request processing time.
+            setTimeout(function() {
+                response.status(403).json({success: false, message: "Authentication failed: bad username or password"});
+            }, 100);
+        } else {
+            user.comparePassword(request.body.password, function(err, isMatch) {
+                if (isMatch && !err) {
+                    // encode the username in a token
+                    var token = jwt.encode(user, config.secret);
+                    // return the token back to the client
+                    response.status(200).json({success: true, token: "JWT " + token});
+                } else {
+                    response.status(403).json({success: false, message: "Authentication failed: bad username or password"});
+                }
+            });
+        }
+    });
+});
+
 app.use("/api", apiRouter);
 
 app.listen(port);
