f1b60b7d5617b8adc8b0d0aaff748e0b38a007f0
Vishal Kotcherlakota <vishal@kotcherlakota.org>
2016-09-26 22:10:18 -0700

    got registration working

---
+++ node/config/passport.js
@@ -0,0 +1,24 @@
+var JwtStrategy = require("passport-jwt").Strategy;
+
+// obtain the user model
+var User = require("../models/user");
+var databaseConfig = require("../config/database");
+
+// this function will take in our passport argument and direct it to use
+// the following strategy for determining authentication
+module.exports = function(passport) {
+    var options = {};
+    options.secretOrKey = databaseConfig.secret;
+    passport.use(new JwtStrategy(options, function(jwtPayload, callback) {
+        User.findOne({id: jwtPayload.id}, function(error, user) {
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        });
+    }));
+};
--- node/server.js
+++ node/server.js
@@ -4,7 +4,7 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var config = require('./config/database');
-var user = require('./models/user');
+var User = require('./models/user');
 var port = process.env.PORT || 8080;
 var jwt = require('jwt-simple');
 var app = express();
@@ -16,12 +16,45 @@ app.use(bodyParser.json());
 // log to the console
 app.use(morgan('dev'));
 
+// set the 
+
 // use the passport module
 app.use(passport.initialize());
 
 app.get('/', function(request, response) {
     response.send('Hi! The application is live.');
 });
+
+// connect to the db
+mongoose.connect(config.database);
+
+// apply the JWT strategy to passport
+require("./config/passport")(passport);
+
+// bundle up routes for API in a single router
+
+var apiRouter = express.Router();
+
+// POST to /api/register to create a new account
+apiRouter.post("/register", function(request, response) {
+    if (!request.body.name || !request.body.password) {
+        response.status(400).json({success: false, message: "request needs username and password"});
+    } else {
+        var newUser = new User({
+            name: request.body.name,
+            password: request.body.password
+        });
+
+        newUser.save(function (err) {
+            if (err) {
+                return response.status(403).json({success: false, message: "user already exists in db"});
+            }
+            response.status(200).json({success: true, message: "Registered new user!"});
+        });
+    }
+});
+
+app.use("/api", apiRouter);
 
 app.listen(port);
 console.log("server active on port " + port);
